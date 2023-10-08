5db57b039fe85ddf78b23ff4f4a09b814b21291d
sathishkumar-sathish <146317947+sathishkumar-sathish@users.noreply.github.com>
2023-10-08 11:06:25 +0200

    Create data1.py

---
+++ data1.py
@@ -0,0 +1,7 @@
+unique_values = []
+for col in object_cols:
+  unique_values.append(dataset[col].unique().size)
+plt.figure(figsize=(10,6))
+plt.title('No. Unique values of Categorical Features')
+plt.xticks(rotation=90)
+sns.barplot(x=object_cols,y=unique_values)
